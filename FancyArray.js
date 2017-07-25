1a6c4f3aa85ee187309d78ab38523cc0d8ce4b5b
JamesORourke <geekjamesorourkejr@icloud.com>
2017-07-25 12:40:29 -0700

    added reverse optional optional

--- FancyArray.js
+++ FancyArray.js
@@ -1,6 +1,12 @@
-function FancyArray(arr, text) {
-  for (var k = 0; k < arr.length; k++) {
-    console.log(k + " " + text + " " + arr[k]);
+function FancyArray(arr, text, reversed=false) {
+  if (reversed) {
+    for (var k = arr.length - 1; k >= 0; k--) {
+      console.log(k + " " + text + " " + arr[k]);
+    }
+  } else {
+    for (var k = 0; k < arr.length; k++) {
+      console.log(k + " " + text + " " + arr[k]);
+    }
   }
 }
 
@@ -12,3 +18,6 @@ FancyArray( [], '**' );
 
 console.log("Single item list test with '::'. ")
 FancyArray( ['James'], '::' );
+
+console.log("Reversed 4 value test with '->'. ");
+FancyArray( ['James','Jill','Jean','Jack'], ' -> ', true );
